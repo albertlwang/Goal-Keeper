1a008fe7fb6bd2e4d9b85fe4346183a8b75d27a6
albertlwang <124311717+albertlwang@users.noreply.github.com>
2026-06-10 18:55:56 -0400

    Initial Commit

---
+++ Goal Keeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Goal Keeper
+//
+//  Created by Albert Wang on 6/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Goal Keeper/Goal_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_KeeperApp.swift
+//  Goal Keeper
+//
+//  Created by Albert Wang on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Goal_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
